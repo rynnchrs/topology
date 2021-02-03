feb71c209b6b8fbcf69b313fb981a3e97a13f85b
Sydrick Frisco <syd.frisco@gmail.com>
2021-02-03 12:27:47 +0800

    added permission and logout

--- backend/careta/urls.py
+++ backend/careta/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
-from rest_framework_simplejwt import views as jwt_views
-from .views import RegisterView, BlacklistTokenView
+from django.urls import path # add this
+from rest_framework_simplejwt import views as jwt_views # add this
+from .views import RegisterView, BlacklistTokenView # add this
 urlpatterns = [
-    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('logout/blacklist/', BlacklistTokenView.as_view(), name='blacklist'),
-    path('register/', RegisterView.as_view(), name='register')
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'), # add this
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'), # add this
+    path('logout/blacklist/', BlacklistTokenView.as_view(), name='blacklist'), # add this
+    path('register/', RegisterView.as_view(), name='register') # add this
 ]
