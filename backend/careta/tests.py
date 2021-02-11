119c2ef4c5eef67fa7e472fccc7423e2ff2ad0d3
Sydrick Frisco <syd.frisco@gmail.com>
2021-02-11 09:29:53 +0800

    fix search filter

--- backend/careta/tests.py
+++ backend/careta/tests.py
@@ -2,7 +2,7 @@ import json
 
 from django.contrib.auth.models import User
 from django.urls import reverse
-from rest_framework import status
+from rest_framework import serializers, status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 
@@ -39,3 +39,61 @@ class RegistrationTestCase(APITestCase):
         url = reverse('register')
         response = self.client.post(url, data, format='json')
         self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+
+# class CreateUsersTestCase(APITestCase):
+
+#     url = reverse('users-list')
+
+#     def setUp(self):
+#         self. data = {
+#                 "username": "test",
+#                 "email": "test@email.com",
+#                 "first_name": "test",
+#                 "last_name": "test",
+#                 "password": "test123!",
+#                 "user_info": {
+#                     "company": "test",
+#                     "position": "test",
+#                     "gender": "M",
+#                     "birthday": "1998-02-07",
+#                     "phone": "123",
+#                     "address": "test"
+#                 }
+#                }
+#     def test_report_create(self):
+#         response = self.client.post(self.url, data=json.dumps(self.data), content_type='application/json')
+#         self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+
+class CreateReportTestCase(APITestCase):
+
+    url = reverse('report-list')
+
+    def setUp(self):
+        Car.objects.create(slug= "1",)
+        car = Car.objects.get(car_id=1)
+        self.data = {
+                    "car": car,
+                    "body_no": "1",
+                    "make": "1",
+                    "mileage": 1,
+                    "location": "1",
+                    "notes": "1",
+                    }
+    def test_report_create(self):
+        response = self.client.post(self.url, data=json.dumps(self.data), content_type='application/json')
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+    def test_report_list(self):
+        response = self.client.get(self.url)
+        reports = Report.objects.all()
+        serializers = ReportSerializer(reports, many=True)
+        self.assertEqual(response.data, serializers.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_report_detail(self):
+        response = self.client.get(self.url, kwargs={'pk': self.data.pk})
+        report = Report.objects.get(pk=self.data.pk)
+        serializer = ReportSerializer(report)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- backend/careta/views.py
+++ backend/careta/views.py
@@ -342,14 +342,14 @@ class ReportView(viewsets.ModelViewSet):  # report Form
                     'interior_lights','screen_washer','wiper_blades','horn','radio','front_fog_lights','air_conditioning','cleanliness_engine_bay',
                     'washer_fluid','coolant_level','brake_fluid_level','power_steering_fluid','gas_level','oil_level','tyres','front_visual',
                     'rear_visual','spare_visual','wheel_brace','jack','front_right_wheel','front_left_wheel','rear_right_wheel','rear_left_wheel', 
-                    'notes','date_updated','date_created']   # filtering
+                    'notes','date_updated','date_created']    # filtering
     ordering_fields = ['car', 'date_created'] # ordering
 
     def create(self, request): # create report 
         serializer = ReportSerializer(data=request.data) 
         if serializer.is_valid(raise_exception=True): 
             serializer.save() # add this
-            return Response("Successfully Register") 
+            return Response(status.HTTP_201_CREATED) 
         return Response(serializer.errors) 
             
 class CarView(viewsets.ModelViewSet):  # add this
