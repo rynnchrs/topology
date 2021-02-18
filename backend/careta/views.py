4f518a677e59bf32297375b175a63c2b03f3a058
rynnchrs <ryannchristopherang@gmail.com>
2021-02-18 03:48:56 -0800

    Commit 18/02/2021-updated first phase

--- backend/careta/views.py
+++ backend/careta/views.py
@@ -331,18 +331,20 @@ class AddRepairReportView(viewsets.ViewSet): # list of can add Repair reports
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)   
 
+
 class ReportView(viewsets.ModelViewSet):  # report Form
 
     queryset = Report.objects.all() 
     serializer_class = ReportSerializer
-    filter_backends = [filters.SearchFilter,filters.OrderingFilter] # filtering and ordering
     search_fields = ['report_id','car__vin_no','body_no','make','mileage','location','cleanliness_exterior','condition_rust','decals','windows',
                     'rear_door','mirror','roof_rack','rear_step','seats','seat_belts','general_condition','vehicle_documents','main_beam',
-                    'dipped_beam','side_lights','tail_lights','indicators','breake_lights','reverse_lights','hazard_light','rear_fog_lights',
+                    'dipped_beam','side_lights','tail_lights','indicators','break_lights','reverse_lights','hazard_light','rear_fog_lights',
                     'interior_lights','screen_washer','wiper_blades','horn','radio','front_fog_lights','air_conditioning','cleanliness_engine_bay',
                     'washer_fluid','coolant_level','brake_fluid_level','power_steering_fluid','gas_level','oil_level','tyres','front_visual',
                     'rear_visual','spare_visual','wheel_brace','jack','front_right_wheel','front_left_wheel','rear_right_wheel','rear_left_wheel', 
                     'notes','date_updated','date_created']   # filtering
+
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]  # filtering and ordering
     ordering_fields = ['car', 'date_created'] # ordering
 
     def create(self, request): # create report 
