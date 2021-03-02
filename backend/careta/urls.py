cf21c18dfa1ed3b3de113e4761c8b8c8705ffbac
sydriiick <syd.frisco@gmail.com>
2021-03-02 11:32:23 +0800

    urls

--- backend/careta/urls.py
+++ backend/careta/urls.py
@@ -1,16 +1,13 @@
 from django.urls import path, include # add this
 from rest_framework_simplejwt import views as jwt_views # add this
-<<<<<<< HEAD
-from .views import RegisterView, BlacklistTokenView , PermissionUserView, ExpiryView # add this
-=======
-from .views import RegisterView, BlacklistTokenView , PermissionUserView, SearchInventoryView # add this
->>>>>>> 4caf1cb559e5500b3d4c723f04e7259518ca75ff
+from .views import RegisterView, BlacklistTokenView , PermissionUserView, ExpiryView# add this
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'), # login api
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'), # refresh token
     path('logout/blacklist/', BlacklistTokenView.as_view(), name='blacklist'), # logout api
     path('register/', RegisterView.as_view(), name='register'), # register api
-    # path('tools/', Tools.as_view(), name='tools') # report creation api
-    path('expiry/', ExpiryView.as_view(), name='expiry-2019') # 
+
+    # path('tools/', Get_with_date.as_view(), name='tools'), # register api
+     path('expiry/', ExpiryView.as_view(), name='expiry-2019') # 
 ]
