94569dc2780bb6ef27e1332df55c3df60b4147fb
Sydrick Frisco <syd.frisco@gmail.com>
2021-03-09 14:26:27 +0800

    03-09 fix

--- backend/careta/migrations/0007_auto_20210216_1258.py
+++ backend/careta/migrations/0007_auto_20210216_1258.py
@@ -6,10 +6,6 @@ import django.db.models.deletion
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('careta', '0006_report_reportimage'),
-    ]
-
     operations = [
         migrations.AlterField(
             model_name='reportimage',
