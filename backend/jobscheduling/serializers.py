15e6f80b1cda61928fbb7cc383d537f5da306d31
ArvinMandap <dead.cardinal@gmail.com>
2021-04-09 03:20:12 +0800

    revised serializers as requested

--- backend/jobscheduling/serializers.py
+++ backend/jobscheduling/serializers.py
@@ -11,17 +11,9 @@ class TaskListSerializer(serializers.ModelSerializer):
     manager_un = serializers.CharField(read_only=True, source='user.username')
     car_vn = serializers.CharField(read_only=True, source='car.vin_no')
     job_order = serializers.CharField(read_only=True, source='joborder.job_number')
-    job_desc = serializers.CharField(max_length=100)
-    job_remarks = serializers.CharField(max_length=100)
     start = serializers.DateField(auto_now=False, auto_now_add=False)
     end = serializers.DateField(auto_now=False, auto_now_add=False)
-    job_startdate_actual = serializers.DateField(auto_now=False, auto_now_add=False)
-    job_enddate_actual = serializers.DateField(auto_now=False, auto_now_add=False)
-    job_actualdays = serializers.IntegerField(default=0, null=True, blank=True)
-    job_status_fm = serializers.CharField(max_length=50)
-    job_status_mn = serializers.CharField(max_length=50)
-    job_scheduledate = serializers.DateField(auto_now=False, auto_now_add=False)
-
+    
     class Meta:
         model = Task
         fields = ['id','manager_un','car_vn','job_order','job_desc','job_remarks',
--- backend/jobscheduling/views.py
+++ backend/jobscheduling/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+import datetime
+from abc import abstractmethod
 
-# Create your views here.
+from django.contrib.auth.models import User  # add this
+from django.db.models import query
+from django.shortcuts import get_object_or_404, render
+from django_filters.rest_framework import DjangoFilterBackend  # filter
+from rest_framework import viewsets  # add this; filter; add this
+from rest_framework import filters, generics, serializers, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response  # add this
+from rest_framework.views import APIView  # add this
+from rest_framework_simplejwt.tokens import RefreshToken  # add this
+from rest_framework_simplejwt.views import TokenObtainPairView
+from reversion.models import Version
+
+from .models import (TPL, Car, Contract, Inspection, Insurance,  # add this
+                     Maintenance, Permission, Repair, Task, UserInfo)
+from .serializers import (TaskListSerializer,JobOrderSerializer,FieldmanAssignmentSerializer)
+from careta.utils import (user_permission)
+
+
+class TaskView(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = Task.objects.all()
+    serializer_class = TaskListSerializer
