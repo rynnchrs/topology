d92040f6491605ef461c53d3fb3fe6847e96179e
sydriiick <syd.frisco@gmail.com>
2021-07-30 16:07:57 +0800

    unique id in image

--- backend/image/utils.py
+++ backend/image/utils.py
@@ -3,19 +3,19 @@ def upload_path(instance, filename):
     if instance.mode == 'cu':
         ext = filename.split('.')[-1]
         filename = '{}.{}'.format(instance.image_name, ext)
-        return '/'.join(['Careta Users/',filename])
+        return '/'.join(['Careta-Users/',filename])
     elif instance.mode == 'ci':
         ext = filename.split('.')[-1]
         path = str(instance.image_name)
         filename = '{}.{}'.format(instance.image_id, ext)
-        return '/'.join(['Car Inventory/',path,filename])
+        return '/'.join(['Car-Inventory/',path,filename])
     elif instance.mode == 'dr':
         ext = filename.split('.')[-1]
         path = str(instance.image_name)
         filename = '{}.{}'.format(instance.image_id, ext)
-        return '/'.join(['Driver Reports/',path,filename])
+        return '/'.join(['Driver-Reports/',path,filename])
     elif instance.mode == 'cr':
         ext = filename.split('.')[-1]
         path = str(instance.image_name)
         filename = '{}.{}'.format(instance.image_id, ext)
-        return '/'.join(['Careta Reports/',path,filename])
+        return '/'.join(['Careta-Reports/',path,filename])
