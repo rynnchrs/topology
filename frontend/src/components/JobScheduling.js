8618a8fce5c2168f66c0e544fe5a72eeabbf6be0
paulandrew97 <alborpaulandrew1997@gmail.com>
2021-09-24 10:02:10 +0800

    Checklist record update and fix location ui in jobschedule and fix careta report dropdwon error

--- frontend/src/components/JobScheduling.js
+++ frontend/src/components/JobScheduling.js
@@ -438,7 +438,7 @@ export const JobScheduling = () => {
             let f = v.fieldman.map((x) =>
                 x.field_man
             )
-            return setFullCalendarList(fullCalendarList => [...fullCalendarList, {"title": "ID: " + v.job_order.job_id + "\nFIELDMAN: " + f + "\nLOCATION: Sample",
+            return setFullCalendarList(fullCalendarList => [...fullCalendarList, {"title": "ID: " + v.job_order.job_id + "\nFIELDMAN: " + f + "\nLOCATION: " + v.current_loc,
             "start": v.start_date, "end": String(endDate)}]);
         });
     }
@@ -522,7 +522,7 @@ export const JobScheduling = () => {
                                         </p>
                                     </div>
                                     <div className="p-col-12 p-lg-6 p-md-12 p-sm-12">
-                                        <p style={{fontSize: '14px', paddingLeft: '17px', wordBreak:'wrap', textIndent:'-9px'}}><i className="pi pi-map-marker"></i> Metro manila makata quezon city </p>
+                                        <p style={{fontSize: '14px', paddingLeft: '17px', wordBreak:'wrap', textIndent:'-9px'}}><i className="pi pi-map-marker"></i> {jobList.current_loc}</p>
                                     </div>
                                 </div>
                             </div>
